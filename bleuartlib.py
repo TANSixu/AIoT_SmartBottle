7de0a18ea04658d9e6e12a9758377bad0f0a8457
MarsTSX <846030067@qq.com>
2021-07-19 14:25:37 +0800

    try ble success

--- bleuartlib.py
+++ bleuartlib.py
@@ -1,108 +1,200 @@
 import time
+import struct
 from bluetooth import ble
 
 import util
 
 
-
 class MyGATTRequester(ble.GATTRequester):
-
 	_callback = None
-	
-	
-	
+
 	def on_notification(self, handle, data):
-		
 		self._callback(data[0])
-	
-	
-	
-	def on_indication(self, handle, data):				
-		
+
+	def on_indication(self, handle, data):
 		if data != None and len(data) > 2:
-		
 			data = data[2:]
-		
+
 		self._callback(data.decode('utf-8'))
-	
-	
-	
+
 	def set_callback(self, callback):
-	
 		self._callback = callback
 
 
-
 class BleUartDevice:
-
 	address = ''
 	gattRequester = None
 
-	
-	
 	def __init__(self, address):
-		
 		self.address = address
-	
-	
-	
+
 	def connect(self):
-		
 		self.gattRequester = MyGATTRequester(self.address, False)
 		self.gattRequester.connect(True, 'random')
 		time.sleep(1)
-	
-	
-	
+
 	def readTemperature(self):
-		
-		temp = self.gattRequester.read_by_handle(0x27)			
+		temp = self.gattRequester.read_by_handle(0x27)
+		# temp=self.gattRequester.read_by_uuid('e95d9250-251d-470a-a062-fa1922dfa9a8')
 		temp = util.convertMicrobitValue(temp)
 		return temp
-	
-	
-	
+
 	def enable_temperature_receive(self, callback):
-	
 		self.gattRequester.set_callback(callback)
-		self.gattRequester.write_by_handle(0x28, '\x01\x00')
-	
-	
-	
+		self.gattRequester.write_by_handle(0x28, b'\x01\x00')
+
+
 	def disable_temperature_receive(self):
-		
 		self.gattRequester.write_by_handle(0x28, '\x00\x00')
-	
-	
-	
+
+	def readAccelerometer(self):
+		# accelerometer = self.gattRequester.read_by_handle(27)
+		accelerometer = self.gattRequester.read_by_uuid("e95dca4b-251d-470a-a062-fa1922dfa9a8")
+		accelerometer = accelerometer[0]
+
+		x_bytes = bytes([accelerometer[0], accelerometer[1]])
+		y_bytes = bytes([accelerometer[2], accelerometer[3]])
+		z_bytes = bytes([accelerometer[4], accelerometer[5]])
+
+		x_short = struct.unpack('<h', x_bytes)[0]
+		y_short = struct.unpack('<h', y_bytes)[0]
+		z_short = struct.unpack('<h', z_bytes)[0]
+
+		return [x_short / 1000.0, y_short / 1000.0, z_short / 1000.0]
+
 	def send(self, strData):
-	
 		txData = util.marshalStringForBleUartSending(strData)
 		self.gattRequester.write_by_handle(0x2a, txData)
 		time.sleep(1)
 
-	# def receive(self):
-	# 	rxd = self.gattRequester.read_by_handle(0x27)
-	# 	rxd = util.convertMicrobitValue(rxd)
-	# 	return rxd
-	
 	def enable_uart_receive(self, callback):
-	
 		self.gattRequester.set_callback(callback)
 		self.gattRequester.write_by_handle(0x28, '\x02\x00')
-		# self.gattRequester.write_by_handle(0x28, '\x02')
 
 	def disable_uart_receive(self, callback):
-	
 		self.gattRequester.set_callback(callback)
 		self.gattRequester.write_by_handle(0x28, '\x00\x00')
-	
-	
+
 	def disconnect(self):
-	
 		if self.gattRequester != None and self.gattRequester.is_connected():
-		
 			time.sleep(1)
-			self.gattRequester.disconnect()			
+			self.gattRequester.disconnect()
 			self.gattRequester = None
 			time.sleep(1)
+
+# import time
+# from bluetooth import ble
+#
+# import util
+#
+#
+#
+# class MyGATTRequester(ble.GATTRequester):
+#
+# 	_callback = None
+#
+#
+#
+# 	def on_notification(self, handle, data):
+#
+# 		self._callback(data[0])
+#
+#
+#
+# 	def on_indication(self, handle, data):
+#
+# 		if data != None and len(data) > 2:
+#
+# 			data = data[2:]
+#
+# 		self._callback(data.decode('utf-8'))
+#
+#
+#
+# 	def set_callback(self, callback):
+#
+# 		self._callback = callback
+#
+#
+#
+# class BleUartDevice:
+#
+# 	address = ''
+# 	gattRequester = None
+#
+#
+#
+# 	def __init__(self, address):
+#
+# 		self.address = address
+#
+#
+#
+# 	def connect(self):
+#
+# 		self.gattRequester = MyGATTRequester(self.address, False)
+# 		self.gattRequester.connect(True, 'random')
+# 		time.sleep(1)
+#
+#
+#
+# 	def readTemperature(self):
+#
+# 		temp = self.gattRequester.read_by_handle(27)
+#
+# 		# print(temp)
+# 		temp = util.convertMicrobitValue(temp)
+# 		return temp
+#
+#
+# 	def readAcc(self):
+# 		temp = self.gattRequester.read_by_handle(27)
+# 		test=temp[0]
+# 		return temp[0]
+#
+#
+#
+# 	def enable_temperature_receive(self, callback):
+#
+# 		# self.gattRequester.set_callback(callback)
+# 		self.gattRequester.write_by_handle(0x28, '\x01\x00')
+#
+#
+#
+# 	def disable_temperature_receive(self):
+#
+# 		self.gattRequester.write_by_handle(0x28, '\x00\x00')
+#
+#
+#
+# 	def send(self, strData):
+#
+# 		txData = util.marshalStringForBleUartSending(strData)
+# 		self.gattRequester.write_by_handle(0x2a, txData)
+# 		time.sleep(1)
+#
+# 	# def receive(self):
+# 	# 	rxd = self.gattRequester.read_by_handle(0x27)
+# 	# 	rxd = util.convertMicrobitValue(rxd)
+# 	# 	return rxd
+#
+# 	def enable_uart_receive(self, callback):
+#
+# 		self.gattRequester.set_callback(callback)
+# 		self.gattRequester.write_by_handle(0x28, '\x02\x00')
+# 		# self.gattRequester.write_by_handle(0x28, '\x02')
+#
+# 	def disable_uart_receive(self, callback):
+#
+# 		self.gattRequester.set_callback(callback)
+# 		self.gattRequester.write_by_handle(0x28, '\x00\x00')
+#
+#
+# 	def disconnect(self):
+#
+# 		if self.gattRequester != None and self.gattRequester.is_connected():
+#
+# 			time.sleep(1)
+# 			self.gattRequester.disconnect()
+# 			self.gattRequester = None
+# 			time.sleep(1)
--- main.py
+++ main.py
@@ -15,94 +15,94 @@
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-import time
-from bluetooth import ble
-
-import util
-from bleuartlib import BleUartDevice
-
-
-def addBleUartDevice(address, name):
-    bleUartDevice = BleUartDevice(address)
-    bleUartDevice.connect()
-    bleUartDevice.enable_uart_receive(bleUartReceiveCallback)
-
-    bleUartDevices.append({'device': bleUartDevice, 'name': name})
-
-
-def bleUartReceiveCallback(data):
-    print('Received data = {}'.format(data))
-    # return data
-
-def sendCommandToAllBleUartDevices(command):
-    for bled in bleUartDevices:
-        bled['device'].send(command)
-
-
-def disconnectFromAllBleUartDevices():
-    for bled in bleUartDevices:
-        bled['device'].disconnect()
-        bled['device'] = None
-
-
-try:
-
-    bleUartDevices = []
-
-    service = ble.DiscoveryService()
-    devices = service.discover(2)
-
-    print('********** Initiating device discovery......')
-
-    for address, name in devices.items():
-
-        if address == 'CF:BE:9E:5F:65:DA':
-
-            print('Found BBC micro:bit [vavet]: {}'.format(address))
-            addBleUartDevice(address, 'vavet')
-
-            print('Added micro:bit device...')
-
-        elif address == 'C8:06:B1:B4:66:53':
-
-            print('Found BBC micro:bit [tipov]: {}'.format(address))
-            addBleUartDevice(address, 'tipov')
-
-            print('Added micro:bit device...')
-
-        elif address == 'DF:60:7F:9B:61:F6':
-
-            print('Found BBC micro:bit [popap]: {}'.format(address))
-            addBleUartDevice(address, 'popap')
-
-            print('Added micro:bit device...')
-
-    if len(bleUartDevices) > 0:
-
-        while True:
-            continue
-            # data = bleUartDevices[0]['device'].enable_uart_receive(bleUartReceiveCallback)
-            # response = input('Do you want to transmit command to micro:bit (Y/n) = ')
-            #
-            # if response == 'Y':
-            #     command = input('Enter command to send = ')
-            #     sendCommandToAllBleUartDevices(command)
-            #     print('Finished sending command to all micro:bit devices...')
-            #
-            # time.sleep(0.1)
-
-except KeyboardInterrupt:
-
-    print('********** END')
-
-except:
-
-    print('********** UNKNOWN ERROR')
-
-finally:
-
-    disconnectFromAllBleUartDevices()
-    print('Disconnected from all micro:bit devices')
+# import time
+# from bluetooth import ble
+#
+# import util
+# from bleuartlib import BleUartDevice
+#
+#
+# def addBleUartDevice(address, name):
+#     bleUartDevice = BleUartDevice(address)
+#     bleUartDevice.connect()
+#     bleUartDevice.enable_uart_receive(bleUartReceiveCallback)
+#
+#     bleUartDevices.append({'device': bleUartDevice, 'name': name})
+#
+#
+# def bleUartReceiveCallback(data):
+#     print('Received data = {}'.format(data))
+#     # return data
+#
+# def sendCommandToAllBleUartDevices(command):
+#     for bled in bleUartDevices:
+#         bled['device'].send(command)
+#
+#
+# def disconnectFromAllBleUartDevices():
+#     for bled in bleUartDevices:
+#         bled['device'].disconnect()
+#         bled['device'] = None
+#
+#
+# try:
+#
+#     bleUartDevices = []
+#
+#     service = ble.DiscoveryService()
+#     devices = service.discover(2)
+#
+#     print('********** Initiating device discovery......')
+#
+#     for address, name in devices.items():
+#
+#         if address == 'CF:BE:9E:5F:65:DA':
+#
+#             print('Found BBC micro:bit [vavet]: {}'.format(address))
+#             addBleUartDevice(address, 'vavet')
+#
+#             print('Added micro:bit device...')
+#
+#         elif address == 'C8:06:B1:B4:66:53':
+#
+#             print('Found BBC micro:bit [tipov]: {}'.format(address))
+#             addBleUartDevice(address, 'tipov')
+#
+#             print('Added micro:bit device...')
+#
+#         elif address == 'DF:60:7F:9B:61:F6':
+#
+#             print('Found BBC micro:bit [popap]: {}'.format(address))
+#             addBleUartDevice(address, 'popap')
+#
+#             print('Added micro:bit device...')
+#
+#     if len(bleUartDevices) > 0:
+#
+#         while True:
+#
+#             # data = bleUartDevices[0]['device'].enable_uart_receive(bleUartReceiveCallback)
+#             response = input('Do you want to transmit command to micro:bit (Y/n) = ')
+#
+#             if response == 'Y':
+#                 command = input('Enter command to send = ')
+#                 sendCommandToAllBleUartDevices(command)
+#                 print('Finished sending command to all micro:bit devices...')
+#
+#             time.sleep(0.1)
+#
+# except KeyboardInterrupt:
+#
+#     print('********** END')
+#
+# except:
+#
+#     print('********** UNKNOWN ERROR')
+#
+# finally:
+#
+#     disconnectFromAllBleUartDevices()
+#     print('Disconnected from all micro:bit devices')
 
 # demo uart receive
 
@@ -113,8 +113,113 @@ finally:
 # import util
 # from bleuartlib import BleUartDevice
 #
+# try:
 #
-# def bleUartReceiveCallback(data):
+#     bleUartDevice1 = None
+#     found_microbit = False
+#
+#     service = ble.DiscoveryService()
+#     devices = service.discover(2)
+#
+#     print('********** Initiating device discovery......')
+#
+#     for address, name in devices.items():
+#
+#         found_microbit = False
+#
+#         if address == 'CF:BE:9E:5F:65:DA':
+#             print('Found BBC micro:bit [vavet]: {}'.format(address))
+#             found_microbit = True
+#             break
+#
+#     if found_microbit:
+#
+#         bleUartDevice1 = BleUartDevice(address)
+#         bleUartDevice1.connect()
+#         print('Connected to micro:bit device')
+#
+#         while True:
+#             temp = bleUartDevice1.readTemperature()
+#             print('Temperature = {}'.format(temp))
+#             time.sleep(0.1)
+#
+# except KeyboardInterrupt:
+#
+#     print('********** END')
+#
+# except Exception :
+#     print(Exception.message)
+#     print('********** UNKNOWN ERROR')
+#
+# finally:
+#
+#     if bleUartDevice1 != None:
+#         bleUartDevice1.disconnect()
+#         bleUartDevice1 = None
+#         print('Disconnected from micro:bit device')
+# demo temperature service
+
+
+# import time
+# from bluetooth import ble
+#
+# import util
+# from bleuartlib import BleUartDevice
+#
+# try:
+#
+#     bleUartDevice1 = None
+#     found_microbit = False
+#
+#     service = ble.DiscoveryService()
+#     devices = service.discover(2)
+#
+#     print('********** Initiating device discovery......')
+#
+#     for address, name in devices.items():
+#
+#         found_microbit = False
+#
+#         if address == 'CF:BE:9E:5F:65:DA':
+#             print('Found BBC micro:bit [vavet]: {}'.format(address))
+#             found_microbit = True
+#             break
+#
+#     if found_microbit:
+#
+#         bleUartDevice1 = BleUartDevice(address)
+#         bleUartDevice1.connect()
+#         print('Connected to micro:bit device')
+#
+#         while True:
+#             accelerometer = bleUartDevice1.readAccelerometer()
+#             print('Accelerometer = x={} y={} z={}'.format(accelerometer[0], accelerometer[1], accelerometer[2]))
+#             time.sleep(1)
+#
+# except KeyboardInterrupt:
+#
+#     print('********** END')
+#
+# except Exception:
+#
+#     print('********** UNKNOWN ERROR')
+#     print(Exception.message)
+#
+# finally:
+#
+#     if bleUartDevice1 != None:
+#         bleUartDevice1.disconnect()
+#         bleUartDevice1 = None
+#         print('Disconnected from micro:bit device')
+
+# import time
+# from bluetooth import ble
+#
+# import util
+# from bleuartlib import BleUartDevice
+#
+#
+# def bleTemperatureReceiveCallback(data):
 #     print('Received data = {}'.format(data))
 #
 #
@@ -132,7 +237,7 @@ finally:
 #
 #         found_microbit = False
 #
-#         if address == 'E1:6E:8C:EC:C6:68':
+#         if address == 'CF:BE:9E:5F:65:DA':
 #             print('Found BBC micro:bit [vavet]: {}'.format(address))
 #             found_microbit = True
 #             break
@@ -143,7 +248,7 @@ finally:
 #         bleUartDevice1.connect()
 #         print('Connected to micro:bit device')
 #
-#         data = bleUartDevice1.enable_uart_receive(bleUartReceiveCallback)
+#         bleUartDevice1.enable_temperature_receive(bleTemperatureReceiveCallback)
 #         print('Receiving data...')
 #
 #         while True:
@@ -153,13 +258,82 @@ finally:
 #
 #     print('********** END')
 #
-# except:
+# except Exception:
 #
 #     print('********** UNKNOWN ERROR')
+#     print(Exception.message)
 #
 # finally:
 #
 #     if bleUartDevice1 != None:
+#         # bleUartDevice1.disable_temperature_receive()
 #         bleUartDevice1.disconnect()
 #         bleUartDevice1 = None
 #         print('Disconnected from micro:bit device')
+
+import pygatt
+import time
+import struct
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+import logging
+
+logging.basicConfig()
+logging.getLogger('pygatt').setLevel(logging.DEBUG)
+
+adapter = pygatt.GATTToolBackend()
+rx=[]
+ry=[]
+rz=[]
+
+def handle_data(handle, value):
+    """
+    handle -- integer, characteristic read handle the data was received on
+    value -- bytearray, the data returned in the notification
+    """
+    accelerometer = value
+
+    x_bytes = bytes([accelerometer[0], accelerometer[1]])
+    y_bytes = bytes([accelerometer[2], accelerometer[3]])
+    z_bytes = bytes([accelerometer[4], accelerometer[5]])
+
+    x_short = struct.unpack('<h', x_bytes)[0]
+    y_short = struct.unpack('<h', y_bytes)[0]
+    z_short = struct.unpack('<h', z_bytes)[0]
+
+
+    plt.ion()
+    
+    rx.append(x_short)
+    ry.append(y_short)
+    rz.append(z_short)
+    plt.clf()
+    plt.plot(rx,label='x')
+    plt.plot(ry, label='y')
+    plt.plot(rz, label='z')
+    plt.legend()
+    plt.show()
+    plt.pause(0.1)
+    # sys.exit(0)
+    print('Accelerometer = x={} y={} z={}'.format(x_short, y_short, z_short))
+
+
+def temp_handle(handle, value):
+    print(f'received: {value}')
+
+try:
+    adapter.start()
+    device = adapter.connect('CF:BE:9E:5F:65:DA', address_type=pygatt.BLEAddressType.random, auto_reconnect=True)
+    print('found')
+    time.sleep(15)
+    # device = adapter.connect('CF:BE:9E:5F:65:DA', address_type=pygatt.BLEAddressType.random, auto_reconnect=True)
+    print('connected')
+    # device.subscribe("e95d9250-251d-470a-a062-fa1922dfa9a8",callback=temp_handle)
+    while True:
+        num = device.char_read_handle('0x27')
+        handle_data(handle='0x27', value=num)
+        time.sleep(0.05)
+   
+finally:
+    adapter.stop()
