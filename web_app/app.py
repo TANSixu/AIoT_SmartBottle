33515520f39ef6818645f4d60ff7b2f8fe884ed5
MarsTSX <846030067@qq.com>
2021-07-26 12:54:01 +0800

    extend web

--- web_app/app.py
+++ web_app/app.py
@@ -3,33 +3,19 @@ import sys
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2 as psy
+import time
 
-WIN = sys.platform.startswith('win')
-if WIN:  # 如果是 Windows 系统，使用三个斜线
-    prefix = 'sqlite:///'
-else:  # 否则使用四个斜线
-    prefix = 'sqlite:////'
+db = psy.connect(database='Smart_bottle', user='postgres', password='123456', host='192.168.3.116',
+                         port='5432')
+cur = db.cursor()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-# 在扩展类实例化前加载配置
-db = SQLAlchemy(app)
 
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字
-
-class Movie(db.Model):  # 表名将会是 movie
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    title = db.Column(db.String(60))  # 电影标题
-    year = db.Column(db.String(4))  # 电影年份
-
-@app.context_processor
-def inject_user():
-    user = User.query.first()
-    return dict(user=user)
-
+class Record():
+    def __init__(self, time, amt):
+        self.time = time
+        self.amt = amt
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -38,39 +24,34 @@ def page_not_found(e):
 
 @app.route('/')
 def index():
-    movies = Movie.query.all()
-    return render_template('index.html', movies=movies, aaa=[2,3,4])
+    # current_time = time.strftime("%Y-%m-%d", time.localtime())
+    current_time = '2021-7-25'
+    cur.execute('''select * from drinking
+                      order by  time desc   
+                      limit 5''')
+    result = cur.fetchall()
+    record = []
+    for r in result:
+        hour = r[0]
+        amt = format(r[1], '.2f')
+        record.append(Record(hour, amt))
+        
+    cur.execute('''select* from daily_report('%s')''' %(current_time))
+    daily = cur.fetchall()
+    freq = []
+    volume = []
+    for dr in daily:
+        freq.append(dr[0])
+        volume.append(round(dr[1], 3))
+
+    return render_template('index.html', freq=freq, vol=volume, records=record)
 
 @app.route('/statistics')
 def statistics():
     movies = Movie.query.all()
     return render_template('statistics.html', movies=movies)
 
-import click
 
-@app.cli.command()
-def forge():
-    """Generate fake data."""
-    db.create_all()
 
-    # 全局的两个变量移动到这个函数内
-    name = 'Username'
-    movies = [
-        {'title': '500ml', 'year': '9:00'},
-        {'title': '250ml', 'year': '10:00'},
-        {'title': '100ml', 'year': '12:00'},
-    ]
-
-    user = User(name=name)
-    db.session.add(user)
-    for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])
-        db.session.add(movie)
-
-    db.session.commit()
-    click.echo('Done.')
-    
-    
-    
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
